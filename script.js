43671bb1fce803b4ae4856aaecf1d6863022a326
ranaasamah16-design <ranaasamah16@gmail.com>
2026-07-08 17:51:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -45,3 +45,37 @@ section.classList.add("hidden");
 observer.observe(section);
 
 });
+
+// =========================
+// COUNTDOWN
+// =========================
+
+const weddingDate = new Date("August 16, 2026 10:30:00").getTime();
+
+
+setInterval(()=>{
+
+const now = new Date().getTime();
+
+const distance = weddingDate - now;
+
+
+const days = Math.floor(distance/(1000*60*60*24));
+
+const hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+
+const minutes = Math.floor((distance%(1000*60*60))/(1000*60));
+
+const seconds = Math.floor((distance%(1000*60))/1000);
+
+
+document.getElementById("days").innerHTML=days;
+
+document.getElementById("hours").innerHTML=hours;
+
+document.getElementById("minutes").innerHTML=minutes;
+
+document.getElementById("seconds").innerHTML=seconds;
+
+
+},1000);
